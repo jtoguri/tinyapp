27f7e0d9d16364889af7994917460d7182e6735d
Joshua Toguri <joshua.toguri@gmail.com>
2022-02-21 10:54:56 -0500

    implemented functionality to update database when a url is entered and added redirect functionality to take the user from short to long

--- express_server.js
+++ express_server.js
@@ -22,8 +22,17 @@ function generateRandomString() {
 }
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  // Generate a random string until one is created that does not already exist in the database
+  let shortURL = '';
+  do {
+    shortURL = generateRandomString();
+  } while (urlDatabase[shortURL])
+  
+  // Add the short and long url key value pair to the database
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/", (req, res) => {
@@ -50,6 +59,11 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL =  urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
