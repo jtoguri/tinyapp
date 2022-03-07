0a794202648a4dde6c8abac53ded4ade8a833bd0
Joshua Toguri <joshua.toguri@gmail.com>
2022-03-07 17:30:06 -0500

    refactoring and editing bootstrap classes

--- express_server.js
+++ express_server.js
@@ -69,42 +69,46 @@ function createNewId(database) {
 
   return id;
 }
-// NOTE** change this function to getUserByEmail()
-function checkEmailExists(email, database) {
+
+// Function that takes in an email and a database of users then retrives the user id of the user that email belongs to
+function getUserByEmail(email, database) {
+  
+  // If a user in the database has the same email as the one provided that user's id is returned
   for (const user in database) {
     if (database[user].email === email) return database[user].id;
   }
-  return false;
+
+  // If no user exists with the email, the function returns null
+  return null;
 }
 
+// Function that returns the urls that belong to a user, given the user id
 function urlsForUser(id) {
   const urls = {};
+  
+  // Loop over the url database and compare each corresponding user id to the given user id
+  // If the user ids match, the url object gets added to the list of urls
   for (const url in urlDatabase) {
     if (urlDatabase[url].userID === id) {
       urls[url] = urlDatabase[url].longURL;
     }
   }
+
   return urls;
 }
 
-// This should probably be changed to a redirect to /urls
-app.get("/", (req, res) => {
-  res.redirect("/urls");
-});
-
-// This should also probably be modified
 app.listen(PORT, () => {
   // console.log(`Example app listening on port ${PORT}!`);
 });
 
-// Should login checks be added in here? Or should this just be removed, no other mention of it after first seen
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// The index page redirects to the urls page
+app.get("/", (req, res) => {
+  res.redirect("/urls");
 });
 
 // Route handler to display the current user's urls
-app.get("/urls", (req, res) => {  
+app.get("/urls", (req, res) => {
+
   // Include the object containing the current user's info and the list of urls that belong to them as template variables
   const templateVars = {
       user: users[req.session.user_id],
@@ -113,35 +117,70 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-// Route handler to display the page for users to add a new url
-app.get("/urls/new", (req, res) => {
-
-  // If the user is not logged in they are redirected to the login page
-  if (!req.session.user_id) res.redirect("/login");
-  // If the user is logged in, include the object containing the current user's info as a template variable
-  const templateVars = { user: users[req.session.user_id] };
-  res.render("urls_new", templateVars);
-});
-
 // Route handler to create a new url and add it to the url datatbase
 app.post("/urls", (req, res) => {
 
+  // Create a unique short url
   const shortURL = createNewId(urlDatabase);
   
-  // Add the short and long url key value pair to the database
+  // Add the short url to the url database
+  // Each short url is an object containing the corresponding long url and the id of the user it belongs to
   urlDatabase[shortURL] = { longURL: req.body.longURL, userID: req.session.user_id };
-  console.log(urlDatabase);
 
   // Redirect the user to the page displaying the long url they just added along with the corresponding short url
   res.redirect(`/urls/${shortURL}`);
 });
 
+// Route handler to display the page for users to add a new url
+app.get("/urls/new", (req, res) => {
+
+  // If the user is not logged in they are redirected to the login page
+  if (!req.session.user_id) res.redirect("/login");
+
+  // If the user is logged in, include the object containing the current user's info as a template variable
+  const templateVars = { user: users[req.session.user_id] };
+  res.render("urls_new", templateVars);
+});
+
 // Route handler to look up a short url in the url database and redirect users to the corresponding long url
-// *** should some type of error check be added here to ensure the short url exists? ****
 app.get("/u/:shortURL", (req, res) => {
+
+  // If the specified short url does not exist, the user is redirected to the home page
+  if (!urlDatabase[req.params.shortURL]) res.redirect("/urls");
+
+  // If the shor url exists the user is redirected to the long url
   const longURL =  urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
+
+// Rout handler to view info about a provided short url, as well as edit the long url if the url belongs to the user
+app.get("/urls/:shortURL", (req, res) => {
+
+  // If the short url does not exist in the database, redirect the user to the home page
+  if (!urlDatabase[req.params.shortURL]) res.redirect("/urls");
+  
+  // Include the short/long urls, the object containing the user's info, and the list of the user's urls as template variables
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL].longURL,
+    user: users[req.session.user_id],
+    urls: urlsForUser(req.session.user_id)
+  };
+  res.render("urls_show", templateVars);
+});
+
+// Route handler to allow users to update a url from the app
+app.post("/urls/:shortURL", (req, res) => {
+
+  // Access the urls that belong to the current user
+  const urls = urlsForUser(req.session.user_id);
+
+  // If the url to be updated belongs to the current user then update it
+  if (urls[req.params.shortURL]) urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+
+  // Redirect the user to the home page to see their full url list
+  res.redirect("/urls");
+})
 
 // Route handler to delete a url from the url database
 app.post("/urls/:shortURL/delete", (req, res) => {
@@ -155,20 +194,11 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect("/urls");
 })
 
-// Route handler to allow users to update a url from the app
-app.post("/urls/:shortURL", (req, res) => {
-  // Access the urls that belong to the current user
-  const urls = urlsForUser(req.session.user_id);
-  // Only update the short url to the new long url if it belongs to the current user
-  if (urls[req.params.shortURL]) urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect("/urls");
-})
-
 // ****** NOTE when using curl to login (from cookies in express) with -d "email=" -d "password=" and -L it ends up in an infinite loop redirecting to /urls, why is this?
 // Route handler to allow users to login to the app
 app.post("/login", (req, res) => {
   // Confirm the user has inputed an existing email
-  const userId = checkEmailExists(req.body.email, users);
+  const userId = getUserByEmail(req.body.email, users);
   // If the user did not input a valid email or password then a 'forbidden' HTTP response status code is sent, indicating the user is un-authorized
   if (!userId || !bcrypt.compareSync(req.body.password, users[userId].password)) res.sendStatus(403);
   // If the user is valid, set the user_id cookie to be the user id corresponding to the provided email
@@ -186,7 +216,12 @@ app.post("/logout", (req, res) => {
 // ***** Should there be a conditional here so that users who are already logged in cannot register new users?*****
 app.get("/register", (req, res) => {
   // Include the object containing the current user's info as a template variable
-  const templateVars = { user: users[req.session.user_id] };
+
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  }
+  
+  const templateVars = { user: null };
   res.render("registration_form", templateVars)
 });
 
@@ -195,7 +230,7 @@ app.post("/register", (req, res) => {
   
   // Check if email/password are empty, or if the email already exists
   // *NOTE potential refactoring this at later point, specifically test out res.status options compared to res.sendStatus
-  if (req.body.email.length === 0 || req.body.password.length === 0 || checkEmailExists(req.body.email, users)) {
+  if (req.body.email.length === 0 || req.body.password.length === 0 || getUserByEmail(req.body.email, users)) {
     res.sendStatus(400);
   }
   
@@ -214,21 +249,15 @@ app.post("/register", (req, res) => {
   res.redirect('/urls');
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  
-  // Include the short/long urls, the object containing the user's info, and the list of the user's urls as template variables
-  const templateVars = { 
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
-    user: users[req.session.user_id],
-    urls: urlsForUser(req.session.user_id)
-  };
-  res.render("urls_show", templateVars);
-});
-
+// Route handler to render the login page
 app.get("/login", (req, res) => {
   // Include the the object containing the current user's info as a template variable
   // *** NOTE should therer be a pre check here to see if the user is already logged in and if so not allow them to access the login page?
-  const templateVars = { user: users[req.session.user_id] };
-  res.render("login_form", templateVars)
+
+  if (req.session.user_id) {
+    res.redirect("/urls");
+  }
+  
+  const templateVars = { user: null };
+  res.render("login_form", templateVars);
 })
